5af3d48d31cc73f1d75a5f73bd83bd3388a19300
Andrea Carter <40808764+mirtacarter@users.noreply.github.com>
2018-07-30 20:42:03 -0400

    clarified comments

--- assets/app.js
+++ assets/app.js
@@ -8,12 +8,15 @@ function renderButtons() {
     // Clear content in buttons div
       $("#new-buttons").empty();
     
-    // Loop over array to generate button, add attributes, append to div
+    // Loop function over array
       for (i = 0; i < topics.length; i++){
+        // create variable for new button
         var newButton = $("<button>");
+        // add class and attributes
         newButton.addClass("housewife");
         newButton.attr("data-name", topics[i]);
         newButton.text(topics[i]);
+        // append to div
         $("#new-buttons").append(newButton);
       }
     }
@@ -28,6 +31,9 @@ function renderButtons() {
     
       // Push to array
       topics.push(newPerson);
+
+      // Clear input field
+      $("#name-input").val('');
     
       // Call function to render buttons
       renderButtons();
@@ -36,40 +42,64 @@ function renderButtons() {
 // Function to get gif results
 function displayGifs (){
 
+    // variables for query
     var name = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/random/search?q=" +
-      name + "&api_key=dc6zaTOxFJmzC&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      name + "&api_key=dc6zaTOxFJmzC&limit=10&rating=g";
 
-      console.log(name);
-
+    // ajax request
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
 
     var results = response.data;
-    
 
     for (var i = 0; i < results.length; i++) {
+        // new div for gif results
         var newDiv = $("<div class='gifs'>");
         var p = $("<p>");
+        // show rating
         p.text("Rating " + results[i].rating);
+        // variables for img tag, still image URL, and animated image URL
         var gifs = $("<img>");
-        gifs.attr("src", results[i].images.fixed_height_still.url);
+        var stillImg = results[i].images.fixed_height_still.url;
+        var animatedImg = results[i].images.fixed_height.url;
+        // initial image src is still image URL
+        gifs.attr("src", stillImg);
+        // added toggle class and data-states for animation function
+        gifs.addClass("toggle");
+        gifs.attr("data-state", "still");
+        gifs.attr("data-still", stillImg);
+        gifs.attr("data-animate", animatedImg);
+        // append items to new div
         newDiv.append(p);
         newDiv.append(gifs);
+        // prepend new div to image bar
         $(".image-bar").prepend(newDiv);
     }
 });
 }
 
+// function to check variable state and toggle animation on and off
+function playGifs(){
+    var state = $(this).attr("data-state");
+    
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    }
 
+// click event listeners for generating gifs and toggling animation
 $(document).on("click", ".housewife", displayGifs);
+$(document).on("click", ".toggle", playGifs);
 
-// Calling the renderButtons function to display the initial list of movies
+// Calling the renderButtons function 
 renderButtons();
-
-
 
 
 });
